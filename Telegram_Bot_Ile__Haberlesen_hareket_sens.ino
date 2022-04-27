8a1772572b9f4c8362a2d5967a2451e71e138097
Nouman <100492073+noumanimpra@users.noreply.github.com>
2022-04-27 03:19:27 +0300

    Update Telegram_Bot_Ile__Haberlesen_hareket_sens.ino

--- Telegram_Bot_Ile__Haberlesen_hareket_sens.ino
+++ Telegram_Bot_Ile__Haberlesen_hareket_sens.ino
@@ -3,16 +3,14 @@
 #include <UniversalTelegramBot.h>
 #include <ArduinoJson.h>
 #include <WiFi.h>
+
 // Bağlanacağınız WiFi bilgileri. 
 const char* ssid = "WIFI ADI";
 const char* password = "WIFI SIFRE";
 const int led = D10;
 const int pir = D9;
 
-
 int hareket;
-
-
 
 // Telegram BOT bilgileri. 
 #define BOTtoken "00000000000000000000000000000000000000" // Telegram uygulaması üzerinden Botfather ile öğreneceğiz. Burayı değiştirmeyi unutmayın.
@@ -20,7 +18,6 @@ int hareket;
 
 WiFiClientSecure client;
 UniversalTelegramBot bot(BOTtoken, client);
-
 
 void setup() {
   pinMode(led, OUTPUT);
@@ -39,29 +36,23 @@ void setup() {
     Serial.print(".");
     delay(500);
   }
-
   Serial.println("");
   Serial.println("WiFi bağlandı");
   Serial.print("IP adresi: ");
   Serial.println(WiFi.localIP());
-
   bot.sendMessage(CHAT_ID, "Cihaz aktif oldu.", "");
 }
-
 void loop() {
-  
-  hareket = digitalRead(pir);
+hareket = digitalRead(pir);
   if(hareket == HIGH){
-digitalWrite(led, HIGH);
-Serial.print("1");
-bot.sendMessage(CHAT_ID, "hareket algilandi");
+    digitalWrite(led, HIGH);
+    Serial.print("1");
+    bot.sendMessage(CHAT_ID, "hareket algilandi");
+  }
+  else{
+    digitalWrite(led, LOW);
+    Serial.print("0");
 }
-else{
-digitalWrite(led, LOW);
-Serial.print("0");
-}
-
-   Serial.println("G");
-   //delay(1000);
- 
+Serial.println("G");
+//delay(1000); 
 }
